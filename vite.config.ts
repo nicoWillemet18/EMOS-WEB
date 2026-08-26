e9979edb4b99735dc4bacee7909d6c7093da0665
Nicolás Willemet <nwillemet18@gmail.com>
2026-08-26 17:17:27 -0300

    GitPages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  base: '/EMOS-WEB/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
